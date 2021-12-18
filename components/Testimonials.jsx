ed54a6b015d3689a1bbb88c2ced12563eae1cef8
Luiz Tanure <letanure@gmail.com>
2021-12-18 23:35:50 +0100

    feat: add testmonials

---
+++ components/Testimonials.jsx
@@ -0,0 +1,144 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  Container,
+  Avatar,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+const Testimonial = ({ children }) => {
+  return <Box>{children}</Box>;
+};
+
+const TestimonialContent = ({ children }) => {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      boxShadow={"lg"}
+      p={8}
+      rounded={"xl"}
+      align={"center"}
+      pos={"relative"}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: "solid transparent",
+        borderLeftWidth: 16,
+        borderRight: "solid transparent",
+        borderRightWidth: 16,
+        borderTop: "solid",
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue("white", "gray.800"),
+        pos: "absolute",
+        bottom: "-16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      {children}
+    </Stack>
+  );
+};
+
+const TestimonialHeading = ({ children }) => {
+  return (
+    <Heading as={"h3"} fontSize={"xl"}>
+      {children}
+    </Heading>
+  );
+};
+
+const TestimonialText = ({ children }) => {
+  return (
+    <Text
+      textAlign={"center"}
+      color={useColorModeValue("gray.600", "gray.400")}
+      fontSize={"sm"}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const TestimonialAvatar = ({ src, name, title }) => {
+  return (
+    <Flex align={"center"} mt={8} direction={"column"}>
+      <Avatar src={src} alt={name} mb={2} />
+      <Stack spacing={-1} align={"center"}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+          {title}
+        </Text>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default function WithSpeechBubbles() {
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.700")}>
+      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={"center"}>
+          <Heading>Our Clients Speak</Heading>
+          <Text>We have been working with clients around the world</Text>
+        </Stack>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={{ base: 10, md: 4, lg: 10 }}
+        >
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Quick and easy</TestimonialHeading>
+              <TestimonialText>
+                It was a great experience selling my IG page on this website. I
+                received my payment on time. Thank you for your service, I wish
+                you more success!” -
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80"
+              }
+              name={"Abubakar"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Secure & anonymous</TestimonialHeading>
+              <TestimonialText>
+                They helped me to get my money with western union. Im gowring
+                more Instagram accounts to sell here. Thanks!”
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80"
+              }
+              name={"Mark K"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Quick payment</TestimonialHeading>
+              <TestimonialText>
+                They helped me to sell my page in 1 weeks. After the payment I
+                got my money in one day over PayPal”
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+              }
+              name={"Jane Cooper"}
+            />
+          </Testimonial>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
