3ef448737df625d146ce1f34719aa682712cdfd1
Luiz Tanure <letanure@gmail.com>
2021-12-19 22:06:37 +0100

    add login

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -10,15 +10,118 @@ import {
   Button,
   Heading,
   Text,
+  FormHelperText,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import Joi from "joi";
+import { useState } from "react";
+import { db } from "../services/firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const schema = Joi.object({
+  email: Joi.string()
+    .email({
+      tlds: {
+        allow: false,
+      },
+    })
+    .required(),
+  password: Joi.string().min(6).required(),
+});
 
 export default function SimpleCard() {
   const router = useRouter();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorGeneral, setErrorGeneral] = useState();
 
-  const handleSubmit = (e) => {
-    router.push("/dashboard");
+  const saveToDb = async (newdata) => {
+    try {
+      const docRef = await addDoc(collection(db, "signins"), newdata);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const logIn = async (email, password) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        saveToDb({
+          uid: user.uid,
+          name,
+          password,
+          authProvider: "local",
+          email,
+        });
+        setErrorGeneral("");
+        router.push("/dashboard");
+      })
+      .catch((error) => {
+        if (error.code === "auth/too-many-requests") {
+          setErrorGeneral(error.message);
+        }
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "EMAIL_NOT_FOUND"
+        ) {
+          setErrorGeneral("User not found");
+        }
+        if (error.code === "auth/wrong-password") {
+          setErrorGeneral("Wrong password or email");
+        }
+      });
+  };
+
+  const validate = (key, value) => {
+    let error = "";
+
+    if (key === "password") {
+      if (value.length < 6) {
+        error = "Password need at least 6 chars ";
+      }
+    }
+    if (key === "email") {
+      if (!/^\S+@\S+\.\S+$/.test(value)) {
+        error = "Email must be valid";
+      }
+    }
+    if (value === "") {
+      error = "Required field";
+    }
+    return error;
+  };
+
+  const handleSubmit = () => {
+    const { error, value } = schema.validate(data);
+    if (error) {
+      const listErrors = {};
+      for (let key in data) {
+        listErrors[key] = validate(key, data[key]);
+      }
+      setErrors(listErrors);
+      return;
+    } else {
+      logIn(data.email, data.password);
+    }
+  };
+
+  const handleChange = async (key, value) => {
+    setData((prevdata) => ({
+      ...prevdata,
+      [key]: value,
+    }));
+    setErrors({ ...errors, [key]: validate(key, value) });
   };
   return (
     <Flex
@@ -43,12 +146,28 @@ export default function SimpleCard() {
           <Stack spacing={4}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                value={data.email}
+                onChange={(e) => handleChange("email", e.target.value)}
+              />
+              <FormHelperText color={"red.400"}>
+                {errors.email ? errors.email : ""}
+              </FormHelperText>
             </FormControl>
+
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input
+                type="password"
+                value={data.password}
+                onChange={(e) => handleChange("password", e.target.value)}
+              />
+              <FormHelperText color={"red.400"}>
+                {errors.password ? errors.password : ""}
+              </FormHelperText>
             </FormControl>
+
             <Stack spacing={10}>
               <Stack
                 direction={{ base: "column", sm: "row" }}
@@ -60,6 +179,7 @@ export default function SimpleCard() {
                   Forgot password?
                 </Link>
               </Stack>
+              {errorGeneral && <Text color={"red.400"}>{errorGeneral}</Text>}
               <Button
                 bg={"green.400"}
                 color={"white"}
