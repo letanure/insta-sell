118d51200d61277109761c75ddeed1909274163a
Luiz Tanure <letanure@gmail.com>
2021-12-19 03:31:07 +0100

    add page support

---
+++ pages/support.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Box } from "@chakra-ui/react";
+
+import Sidebar from "../components/Sidebar";
+import ListAccounts from "../components/ListAccounts";
+
+export default function Home() {
+  return (
+    <Box width="100%">
+      {/* <Header /> */}
+      <Sidebar>Support</Sidebar>
+    </Box>
+  );
+}
