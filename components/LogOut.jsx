756b36f1637db5c06795185c033508985381c89b
Luiz Tanure <letanure@gmail.com>
2021-12-19 09:51:48 +0100

    fix height

--- components/LogOut.jsx
+++ components/LogOut.jsx
@@ -3,7 +3,7 @@ import { CheckCircleIcon } from "@chakra-ui/icons";
 
 export default function Success() {
   return (
-    <Box textAlign="center" py={10} px={6}>
+    <Box textAlign="center" py={10} px={6} minH={"75vh"}>
       <CheckCircleIcon boxSize={"50px"} color={"green.500"} />
       <Heading as="h2" size="xl" mt={6} mb={2}>
         Logged out successfully!
