4ad1f2feddd2721ee36bef02edbd43214bf9986b
Luiz Tanure <letanure@gmail.com>
2021-12-19 01:29:44 +0100

    add page dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Box } from "@chakra-ui/react";
+
+import Sidebar from "../components/Sidebar";
+import Statistics from "../components/Statistics";
+
+export default function Home() {
+  return (
+    <Box width="100%">
+      {/* <Header /> */}
+      <Sidebar>
+        <Statistics />
+      </Sidebar>
+    </Box>
+  );
+}
