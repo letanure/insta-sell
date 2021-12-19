db07e3891de44860d394349a719ba7a3de038aef
Luiz Tanure <letanure@gmail.com>
2021-12-19 22:35:06 +0100

    prevent show protected pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,40 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import "../services/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { UserContext } from "../components/user";
+import NoAccess from "../components/NoAccess";
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("signed in", user);
+        const uid = user.uid;
+        setUser(user);
+      } else {
+        console.log("NOT signed in");
+      }
+    });
+  }, []);
+
+  if (pageProps.protected && !user) {
+    return (
+      <ChakraProvider>
+        <NoAccess />
+      </ChakraProvider>
+    );
+  }
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <UserContext.Provider value={user}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserContext.Provider>
   );
 }
 
