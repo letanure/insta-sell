13956e2bee01171013b7ff95f2ec5183c0fdcdde
Luiz Tanure <letanure@gmail.com>
2021-12-19 22:06:28 +0100

    add logout

--- components/LogOut.jsx
+++ components/LogOut.jsx
@@ -1,7 +1,22 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
+import "../services/firebase";
+import { getAuth, signOut } from "firebase/auth";
+import { useEffect } from "react";
 
 export default function Success() {
+  const auth = getAuth();
+  useEffect(() => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.info("Sign-out successful.");
+      })
+      .catch((error) => {
+        console.error(error);
+        // An error happened.
+      });
+  });
   return (
     <Box textAlign="center" py={10} px={6} minH={"75vh"}>
       <CheckCircleIcon boxSize={"50px"} color={"green.500"} />
